9d52485270027ec1070d74ad782fec779de808fb
akshatvermavi <akshatvermanike@gmail.com>
2025-08-07 13:14:09 +0530

    backend-first-commit

---
+++ leafTicketingBackend/src/main/java/com/akshat/leafTicketingBackend/LeafTicketingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.akshat.leafTicketingBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafTicketingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeafTicketingBackendApplication.class, args);
+	}
+
+}
---
+++ leafTicketingBackend/src/test/java/com/akshat/leafTicketingBackend/LeafTicketingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshat.leafTicketingBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeafTicketingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
